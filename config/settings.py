b6abe6e5af12242129e164ef83ed54f5e5ec6d04
tsuiml <martintony133@gmail.com>
2026-08-10 15:05:46 +0800

    feat: Step 4

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gamegame',
         'USER' : 'postgres',
-        'PASSWORD' : '',
+        'PASSWORD' : '123456',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
